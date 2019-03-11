11e434a836f7953384669f6bc95f156ef816fdf7
Valentin Geny <vgeny@humanbooster.com>
2019-03-11 16:20:17 +0100

    add x12 and x12 test

--- tests/MultiplyTest.php
+++ tests/MultiplyTest.php
@@ -35,10 +35,10 @@ class MultiplyTest extends TestCase
     }
 	
 	public function testE(){
-		$this->assertSame(36, $this->multiply->multiplyBy6(3));
+		$this->assertSame(36, $this->multiply->multiplyBy12(3));
 	}
 	
 	public function testF(){
-		$this->assertSame(60, $this->multiply->multiplyBy6(5));
+		$this->assertSame(60, $this->multiply->multiplyBy12(5));
 	}
 }
